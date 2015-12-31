3cd7acd729f64b0e12ad70181fe5dae62c6f5107
Matthew Riley MacPherson <matt@lonelyvegan.com>
2015-12-31 16:04:51 +0000

    First version of simple scraper

---
+++ index.js
@@ -0,0 +1,161 @@
+var cheerio = require('cheerio');
+var express = require('express');
+var redis = require('redis');
+var redisClient = redis.createClient();
+var request = require('request');
+
+var BREW_TYPE_MAPPING = {
+  'BREWDOG DRAFT': 'brewDogBeers',
+  'Crafty Devil Takeover': 'craftyDevilTakeover',
+  'GUEST DRAFT': 'guestBeers',
+  'CIDER': 'ciders',
+};
+
+var TIME_TO_EXPIRE = 21600; // six hours
+
+var app = express();
+
+/*
+ * Parse the name of the brewery from a guest ale (otherwise it's BrewDog).
+ */
+function getBreweryAndStrength(brew) {
+  var brewWithData = {};
+  var isBrewDog = false;
+
+  var brewMatches = brew.match(/(.*) - (.*) (\d+\.?\d*%)/);
+  if (!brewMatches) {
+    brewMatches = brew.match(/(.*) (\d+\.?\d*%)/);
+    isBrewDog = true;
+  }
+
+  brewWithData.brewery = isBrewDog ? 'BrewDog' : brewMatches[1].trim();
+  brewWithData.name = isBrewDog ? brewMatches[1].trim() : brewMatches[2].trim();
+  brewWithData.strength = isBrewDog ? brewMatches[2] : brewMatches[3];
+
+  return brewWithData;
+}
+
+/*
+ * Get the ranges of the brew list, used to assign each brew to a section.
+ */
+function getRanges(brewList) {
+  var ranges = {};
+
+  brewList.forEach(function(item, index) {
+    var brewType = item.match(/-\W?(.*)\W?-/);
+    if (brewType) {
+      brewType = brewType[1].trim();
+      ranges[BREW_TYPE_MAPPING[brewType]] = index + 1;
+
+      if (index !== 0) {
+        var lastType = Object.keys(ranges)[Object.keys(ranges).length - 2];
+
+        ranges[lastType] = [ranges[lastType], index - 1];
+      }
+    }
+  });
+
+  var finalKey = Object.keys(ranges)[Object.keys(ranges).length - 1];
+  ranges[finalKey] = [ranges[finalKey], Object.keys(brewList).length - 1];
+
+  return ranges;
+}
+
+/*
+ * Return an object of all brews, according to type.
+ */
+function loadBrewsByType(brewList) {
+  // These are all know types of brews on tap at various bars; some may be
+  // empty.
+  var brews = {
+    brewDogBeers: [],
+    guestBeers: [],
+    ciders: [],
+    craftyDevilTakeover: [],
+  };
+
+  var ranges = getRanges(brewList);
+
+  // Brutely check through each brew type.
+  Object.keys(brews).forEach(function(brewType) {
+    brewList.forEach(function(item, index) {
+      if (index >= ranges[brewType][0] && index <= ranges[brewType][1]) {
+        brews[brewType].push(getBreweryAndStrength(item));
+      }
+    });
+  });
+
+  return brews;
+}
+
+/*
+ * Normalise HTML string into a usable object of tap data.
+ *
+ * We get a big dump of HTML from the web page; this processes out whitespace
+ * and HTML tags into a usable object we can output as JSON.
+ */
+function normalizeOnTapData(string) {
+  var brewList = string.split('<br>')
+    .filter(function(item) {
+      // Remove whitespace items, which crop up because of the HTML contents.
+      return item.trim().length;
+    });
+
+  var brews = loadBrewsByType(brewList);
+
+  return brews;
+}
+
+app.get('/:country/:pub.json', function(req, res) {
+  var country = req.params.country;
+  var pub = req.params.pub;
+  var redisKey = country + '/' + pub;
+
+  // First we'll check for cached data; we cache things for 6 hours.
+  redisClient.get(redisKey, function(err, reply) {
+    if (err) {
+      res.end('Redis failure');
+    }
+
+    var now = new Date().getTime();
+
+    if (reply) {
+      reply = JSON.parse(reply);
+    }
+
+    // No cached data exists or it's outdated; fetch new data!
+    if (!reply || reply.expiryTime < now) {
+      var url = 'https://www.brewdog.com/bars/' + country + '/' + pub;
+
+      request(url, function(err, response, html) {
+        if (err) {
+          return res.end('Failure');
+        }
+
+        var $ = cheerio.load(html);
+
+        var onTapHTML = $('.onTapInfo .barText');
+
+        var beerData = normalizeOnTapData(onTapHTML.html());
+
+        var expiryTime = new Date().getTime() + TIME_TO_EXPIRE;
+        var jsonToSave = JSON.stringify({
+          beerData: beerData,
+          expiryTime: expiryTime,
+        });
+        redisClient.set(redisKey, jsonToSave);
+
+        res.end(JSON.stringify(beerData));
+      });
+    } else {
+      // We have cached data; send it across the wire!
+      res.end(JSON.stringify(reply.beerData));
+    }
+  });
+});
+
+var PORT = process.env.WBD_PORT || '8080';
+
+app.listen(PORT);
+console.log('App running on port ' + PORT);
+exports = module.exports = app;
